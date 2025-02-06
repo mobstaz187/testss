748d3115bb9018b57dda756ddb06e61ba1970ad8
unknown <markmark092100@gmail.com>
2025-02-06 13:59:49 +0800

    first commit

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,16 +1,5 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string
-  readonly VITE_SUPABASE_ANON_KEY: string
-  readonly VITE_DISCORD_CLIENT_ID: string
-  readonly VITE_DISCORD_REDIRECT_URI: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
-
 declare module '*.mp4' {
   const src: string;
   export default src;
